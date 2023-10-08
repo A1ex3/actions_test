c02fd397742539cc05837eac6d3736fb684e275f
A0n1x <pakshin.2018@gmail.com>
2023-10-08 16:32:26 +0700

    new1

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from curses import OK
 from http.client import BAD_REQUEST
 from fastapi.testclient import TestClient
 
-from actions_test.__main__ import app
+from .__main__ import app
 
 client = TestClient(app)
 
