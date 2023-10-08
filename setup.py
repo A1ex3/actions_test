bfc7ccb089e089654f106763176f6d533db40fc0
A0n1x <pakshin.2018@gmail.com>
2023-10-08 16:36:46 +0700

    new1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'fastapi[all]',
+        'pytest',
+    ],
+)
